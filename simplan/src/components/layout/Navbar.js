23e62d99cb7a01e7998bd126907e591c35345a15
Wisuda Dokchompu <wisuda2012@gmail.com>
2019-08-31 23:07:23 +0700

    create Navbar SignInLink and SignOutLink

---
+++ simplan/src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SignInLink from './SignInLink';
+import SignOutLink from './SignOutLink';
+
+
+const Navbar = () =>{
+    return(
+        <nav className ="nav-wrapper grey darken-3">
+            <div className="container">
+            <div className="container">
+                <a href='/' class='brand-logo'>SimplePlan</a>
+                <SignInLink/>
+                <SignOutLink/>
+            </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar 
