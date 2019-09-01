2719aae8c85102484b7c94950408cf6ad83d3dac
Wisuda Dokchompu <wisuda2012@gmail.com>
2019-09-02 00:01:38 +0700

    split ProjectSummary from ProjectList

--- simplan/src/components/projects/ProjectList.js
+++ simplan/src/components/projects/ProjectList.js
@@ -1,42 +1,15 @@
 import React from 'react'
-
+import ProjectSummary from "../projects/ProjectSummary";
 
 
 const ProjectList = () =>{
     return(
         <div className="project-list section">
 
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Post by the Cat of the world</p>
-                    <p className="grey-text">1 st August,2020</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Post by the Cat of the world</p>
-                    <p className="grey-text">1 st August,2020</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Post by the Cat of the world</p>
-                    <p className="grey-text">1 st August,2020</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Post by the Cat of the world</p>
-                    <p className="grey-text">1 st August,2020</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
 
         </div>
     )
---
+++ simplan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Post by the Cat of the world</p>
+                <p className="grey-text">1 st August,2020</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectSummary;
